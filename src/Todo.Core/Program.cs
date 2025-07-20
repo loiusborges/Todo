d952629022633beeeed631976abbdb2013c62684
Luis Fernando <loiusborges@gmail.com>
2025-07-20 00:30:18 -0300

    Configurando Programs.cs

--- src/Todo.Core/Program.cs
+++ src/Todo.Core/Program.cs
@@ -1,6 +1,16 @@
+using Todo.Core.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
